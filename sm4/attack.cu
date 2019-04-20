e937befb5f8b50dee6f8aa538ebff8c31362ef91
yuxudong1512025 <1161431707@qq.com>
2019-04-20 14:02:45 +0800

    稍微快一点的并发，6-0的950m跑40s，16-1的950m跑1分半

--- sm4/attack.cu
+++ sm4/attack.cu
@@ -61,46 +61,72 @@ u8 SboxTable[256] = {
 0x0a,0xc1,0x31,0x88,0xa5,0xcd,0x7b,0xbd,0x2d,0x74,0xd0,0x12,0xb8,0xe5,0xb4,0xb0,
 0x89,0x69,0x97,0x4a,0x0c,0x96,0x77,0x7e,0x65,0xb9,0xf1,0x09,0xc5,0x6e,0xc6,0x84,
 0x18,0xf0,0x7d,0xec,0x3a,0xdc,0x4d,0x20,0x79,0xee,0x5f,0x3e,0xd7,0xcb,0x39,0x48
- };
+};
 
 
 /*(1024*2^14*256*1)*/
 /*限定故障注入在C31第一个字节*/
 /*ciphertxt[2 * n]=C32^C33^C34, ciphertxt[2 * n + 1]=C35    */
-__global__ void kernel( u32 *maxSEI, u32 *ciphertxt, int countn,int p,u8 *Count,u8 *Sbox) {
-	u32 ix = blockIdx.x*blockDim.x + threadIdx.x;
-	u32 key = (p<<16)+ix;
-	__shared__ u8 s[256];
-	u32 MaxSei = 0,ka;
-	u8 a=0x3f,b[4];
+__global__ void kernel(u32 *maxSEI, u32 *maxKey,u32 *ciphertxt, int countn, u8 *Sbox) {//256*65536*256*1
+	u32 ix = threadIdx.x;
+	u32 bdx = blockIdx.y + blockIdx.x*256;
 
-	s[threadIdx.x] = Sbox[threadIdx.x];
+	__shared__ u8 s[256];
+	__shared__ u32 partialKey[256];
+	__shared__ u32 partialSEI[256];
+	u32 MaxSei = 0, ka;
+	u8  b[4],Count[256];
+	
+	s[ix] = Sbox[ix];
+	
 	__syncthreads();
 
+	for (int i = 0; i < 256; i++)Count[i] = 0;
+	///////////////////////////////////////////////
 	for (int i = 0; i < countn; i++) {
-		u32 temp = ciphertxt[2 * i],temp1= ciphertxt[2 * i + 1];
-		ka = key ^ temp;
-		
-		b[0] = s[(u8)(ka>>24)];
-		b[1] = s[(u8)(ka>>16)];
-		b[2] = s[(u8)(ka>>8)];
+		u32 temp = ciphertxt[2 * i], temp1 = ciphertxt[2 * i + 1];
+		ka = ((bdx<<8)+ix) ^ temp;
+
+		b[0] = s[(u8)(ka >> 24)];
+		b[1] = s[(u8)(ka >> 16)];
+		b[2] = s[(u8)(ka >> 8)];
 		b[3] = s[(u8)ka];
 
 		ka = ((u32)b[0] << 24) | ((u32)b[1] << 16) | ((u32)b[2] << 8) | ((u32)b[3]);
-		
-		b[0] = b[0] ^ b[3];
-		b[0] = b[0]^(u8)(ka >> 6) ^ (u8)(ka >> 14) ^ (u8)(ka >> 22) ^ (u8)(temp1>>24);
-		Count[b[0] * 65536 + ix]++;
-	}
-	for (int i = 0; i < 256; i++)
-		MaxSei += doublec(Count[i * 65536 + ix]);
 
-	maxSEI[ix] = MaxSei;
+		b[0] = b[0] ^ b[3];
+		b[0] = b[0] ^ (u8)(ka >> 6) ^ (u8)(ka >> 14) ^ (u8)(ka >> 22) ^ (u8)(temp1 >> 24);
+		Count[b[0]]++;
+	}
+
+
+	for (int i = 0; i < 256; i++)
+		MaxSei += doublec(Count[i]);
+	//////////////////////////////////////////////
+	partialSEI[ix] = MaxSei;
+	partialKey[ix] = ix;
+
+	__syncthreads();
+
+	for (int stride = 256 / 2; stride > 0; stride = stride >> 1) {
+		if (ix < stride) {
+			if (partialSEI[ix] < partialSEI[ix + stride]) {
+				partialSEI[ix] = partialSEI[ix + stride];
+				partialKey[ix] = partialKey[ix + stride];
+			}
+		}
+		__syncthreads();
+	}
+
+	if (ix == 0) {
+		*(maxSEI+bdx) = partialSEI[ix];
+		*(maxKey+bdx) = partialKey[ix];
+	}
 }
 
 
 extern "C"
-u32 getKey_Stream(u32 *ciphertxt0, int Countn, const u32 &trueKey) {
+u32 getKey_Stream(u32 *ciphertxt0, int Countn, u32 &trueKey) {
 
 	FILE *fp = fopen("temp.txt", "a");
 	//输出设备信息可以无视
@@ -110,14 +136,13 @@ u32 getKey_Stream(u32 *ciphertxt0, int Countn, const u32 &trueKey) {
 	printf_s("using device %d : %s \n", dev, deviceProp.name);
 	CHECK(cudaSetDevice(dev));
 	//设置参数
-	int size_16 = 1 << 16;
-	int size_8 = 1 << 8;
-	int size_24 = 1 << 24;
-	int nsize = size_16 * Countn * sizeof(u8);
+	const int size_16 = 1 << 16;
+	const int size_8 = 1 << 8;
+	const int size_24 = 1 << 24;
 	//	printf_s("Matrix size:nx %d ny %d\n", nx, ny);
 
 	//以下为定义的线程布局gird=（256，65536）个block=（256,1）个threads
-	dim3 grid(size_8, 1);
+	dim3 grid(size_16, size_8);
 	dim3 block(size_8, 1);
 	///////////////////////////////////////////////////////
 
@@ -129,33 +154,32 @@ u32 getKey_Stream(u32 *ciphertxt0, int Countn, const u32 &trueKey) {
 
 	CHECK(cudaMemcpy(cipher, ciphertxt0, Countn * 2 * sizeof(u32), cudaMemcpyHostToDevice));
 	//申请device内存作为输出空间（输出）
-	u32 *maxSEI;
-	u8 *Count, *SBOX;
-	CHECK(cudaMalloc((void **)&maxSEI, size_16 * sizeof(u32)));
-	CHECK(cudaMalloc((void **)&Count, size_16 * 256 * sizeof(u8)));
+	u32 *maxSEI,*maxKey;
+	u8 *SBOX;
+	CHECK(cudaMalloc((void **)&maxSEI, size_24 * sizeof(u32)));
+	CHECK(cudaMalloc((void **)&maxKey, size_24 * sizeof(u32)));
 	CHECK(cudaMalloc((void **)&SBOX, 256 * sizeof(u8)));
 
 	CHECK(cudaMemcpy(SBOX, SboxTable, 256 * sizeof(u8), cudaMemcpyHostToDevice));
 	//申请循环结果存放空间
 
-	thrust::device_vector<u32>ansSEI(size_16);
-	thrust::device_vector<u32>ansKEY(size_16);
-	u32 ansS[65536] = { 0 }, ansK[65536] = { 0 }, aS = 0, aK;
 
-
-
-	for (int i = 0; i < size_16; i++) {
-		CHECK(cudaMemset(Count, 0, size_16 * 256 * sizeof(u8)));
 		//调用核函数计算SEI
-		kernel << <grid, block >> > (maxSEI, cipher, Countn, i, Count, SBOX);
-		CHECK(cudaDeviceSynchronize());//检查cuda设备同步情况
+	kernel << <grid, block >> > (maxSEI,maxKey, cipher, Countn,SBOX);
+	CHECK(cudaDeviceSynchronize());//检查cuda设备同步情况
 		//调用核函数求SEI最大的key
 
-		auto ptr = thrust::max_element(thrust::device, maxSEI, maxSEI + 65536);
-		ansSEI[i] = (*ptr);
-		ansKEY[i] = ptr - maxSEI;
 
-	}
+	//u32 *SEIlist = (u32 *)malloc(sizeof(u32)*size_24);
+	//u32 *KEYlist = (u32 *)malloc(sizeof(u32)*size_24);
+	//cudaMemcpy(SEIlist, maxSEI, sizeof(u32)*size_24, cudaMemcpyDeviceToHost);
+	//cudaMemcpy(KEYlist, maxKey, sizeof(u32)*size_24, cudaMemcpyDeviceToHost);
+	//u32 tempit = trueKey>>8;
+	//printf("%d %x\n", SEIlist[tempit],(tempit<<8)+ KEYlist[tempit]);
+	//
+	//free(SEIlist);
+	//free(KEYlist);
+
 	//thrust::host_vector<u32> host(65536),host2(65536);
 	//thrust::copy(ansSEI.begin(), ansSEI.end(), host.begin());
 	//thrust::copy(ansKEY.begin(), ansKEY.end(), host2.begin());
@@ -163,22 +187,23 @@ u32 getKey_Stream(u32 *ciphertxt0, int Countn, const u32 &trueKey) {
 	//	std::fprintf(fp, "%d %x\n", host[i],host2[i]);
 	//}
 
-	auto ptr = thrust::max_element(ansSEI.begin(), ansSEI.end());
+	auto ptr = thrust::max_element(thrust::device, maxSEI, maxSEI + size_24);
 
 	//输出结果用的
 	thrust::device_vector<u32> D(2);
 	thrust::host_vector<u32> H(2);
-	D[0]=(*ptr);
-	D[1]=(((ptr - ansSEI.begin()) << 16) + ansKEY[ptr - ansSEI.begin()]);
-	
+	D[0] = (*ptr);
+	u32 it = (ptr - maxSEI);
+	D[1] = maxKey[it];
+
 	thrust::copy(D.begin(), D.end(), H.begin());
 
-	std::printf("最终找到的最大SEI和对应key：%d %x\n", H[0], H[1]);
+	std::printf("最终找到的最大SEI和对应key：%d %x\n", H[0],(it*256)+H[1]);
 	//释放占用空间
 	CHECK(cudaFree(maxSEI));
+	CHECK(cudaFree(maxKey));
 	CHECK(cudaFree(cipher));
-	CHECK(cudaFree(Count));
 	CHECK(cudaFree(SBOX));
 
-	return H[1];
+	return (it * 256) + H[1];
 }
